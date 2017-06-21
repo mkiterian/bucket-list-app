9877d686e0ec0b5b07aaa554a75d87798843f1ed
Mkiterian <hmukite@gmail.com>
2017-06-21 19:16:44 +0300

    Add route for bucketlist view

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import json
 from user import User
 
 from flask import (Flask, render_template, url_for, 
-        redirect, request, make_response, jsonify)
+        redirect, request, make_response, jsonify, session)
 
 users = {}
 
@@ -15,7 +15,6 @@ def get_saved_data():
         data = {}
     return data
 
-@app.route('/login')
 @app.route('/')
 def index():
     data = get_saved_data()
@@ -32,6 +31,8 @@ def signup():
         if password == confirm_password:
             user = User(username, email, password)
             users['email'] = user
+
+            session['username'] = request.form['username']
             
             return redirect(url_for('manage', data = username))
 
@@ -44,14 +45,10 @@ def signup():
 @app.route('/save', methods=['POST', 'GET'])
 def save():
     if request.method == 'POST':
-        response = make_response(redirect(url_for('manage')))
-        data = get_saved_data()
-        data.update(dict(request.form.items()))
-        response.set_cookie('user', json.dumps(data))#dict(request.form.items())['username'])
+        return redirect(url_for('manage'))
 
-        return response
     else:
-        return render_template('managelists.html', data=data)
+        return render_template('managelists.html')
 
 @app.route('/manage', methods=['POST', 'GET'])
 def manage():
@@ -68,7 +65,11 @@ def manage():
         data = get_saved_data()
         return render_template('managelists.html', data=data)
     
-
+@app.route('/show_bucket', methods=['GET'])
+def show_bucket():
+    return render_template('bucketlist.html')
 
 if __name__ == '__main__':
+    app.secret_key = 'xcxcyuxcyuxcyuxcyxuee'
+
     app.run(debug=True)
